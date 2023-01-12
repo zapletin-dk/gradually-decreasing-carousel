f2d9877061a3b87f5df6697ccfa8a0650628648b
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-13 01:42:42 +0300

    Changed GraduallyDecreasingCarousel constructor parameter capacity from final int to int

--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -1,7 +1,7 @@
 package com.epam.rd.autotasks;
 
 public class GraduallyDecreasingCarousel extends  DecrementingCarousel{
-    public GraduallyDecreasingCarousel(final int capacity) {
+    public GraduallyDecreasingCarousel(int capacity) {
         super(capacity);
     }
 
